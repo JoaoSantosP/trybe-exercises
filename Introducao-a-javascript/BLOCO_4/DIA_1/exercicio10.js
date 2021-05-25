92cd748d9623c6410a165e5bdbe7da48c42d6cdc
Joao Santos <jvitor.spaula@gmail.com>
2021-05-25 12:25:18 -0300

    Corrigindo exercicio 10

--- Introducao-a-javascript/BLOCO_4/DIA_1/exercicio10.js
+++ Introducao-a-javascript/BLOCO_4/DIA_1/exercicio10.js
@@ -1,4 +1,9 @@
 let custo = 200 ;
 let custoFinal = custo /100 * 20 + custo; 
-let venda = (custoFinal - custo) * 1000;
-console.log(venda);
+let lucro = (custoFinal - custo) * 1000;
+
+if(custo < 0 && custoFinal < 0){
+console.log("Error");
+}else{
+  console.log("Seu lucro é de :",lucro);
+};
